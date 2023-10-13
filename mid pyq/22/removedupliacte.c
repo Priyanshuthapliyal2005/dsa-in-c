3d5482aa4586a13c4955113f26b33b3026e5bc4d
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-10-14 00:16:04 +0530

    removeduplicate

---
+++ mid pyq/22/removedupliacte.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int val;
+    struct node* next;
+};
+typedef struct node node;
+void stackinsert(node** head,int val){
+    node* newnode=(node*)malloc(sizeof(node));
+    newnode->val=val;
+    newnode->next=NULL;
+    if(*head){
+        newnode->next=*head;
+    }
+    *head=newnode;
+}
+void stackpopduplicate(node** head) {
+    node* curr = *head;
+    while (curr && curr->next) {
+        if (curr->val == curr->next->val) {
+            node* temp = curr->next;
+            curr->next = temp->next;
+            free(temp);
+        } else {
+            curr = curr->next;
+        }
+    }
+}
+void display(node* head){
+    printf("elements in stack are:\n");
+    node* curr=head;
+    while(curr!=NULL){
+        printf("%d\n",curr->val);
+        curr=curr->next;
+    }
+
+}
+
+int main(){
+    node* head=NULL;
+    stackinsert(&head,1);
+    stackinsert(&head,1);
+    stackinsert(&head,2);
+    stackinsert(&head,3);
+    stackinsert(&head,3);
+    stackinsert(&head,4);
+    stackinsert(&head,4);
+    display(head);
+
+    stackpopduplicate(&head);
+    display(head);
+}

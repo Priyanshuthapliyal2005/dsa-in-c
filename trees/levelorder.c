a46af9460cdc1b79cf098f48775a6fede6b4efbc
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2023-11-17 17:17:22 +0530

    built in order traversal

--- trees/levelorder.c
+++ trees/levelorder.c
@@ -1,6 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct node {
     int data;
@@ -8,22 +8,15 @@ struct node {
     struct node* right;
 };
 
-struct node* createNode(int data) {
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
-}
-
-struct Node{
-    int val;
+struct Node {
+    struct node* data;
     struct Node* next;
 };
 typedef struct Node Node;
-void push(Node** head, int val) {
+
+void push(Node** head, struct node* data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->val = val;
+    newNode->data = data;
     newNode->next = NULL;
 
     if (*head == NULL) {
@@ -37,75 +30,141 @@ void push(Node** head, int val) {
     }
 }
 
-node* pop(Node** head) {
+struct node* pop(Node** head) {
     if (*head == NULL) {
         printf("Queue is empty, cannot pop.\n");
-        return;
+        return NULL;
     }
-    Node* curr=*head;
-    node* key=curr;
-    *head=curr->next;
+    Node* curr = *head;
+    struct node* data = curr->data;
+    *head = curr->next;
     free(curr);
-    return key;
+    return data;
 }
 
-void empty(Node* head){
-    Node* curr=head;
-    if(!head){
-        return;
+int empty(Node* head) {
+    return head == NULL;
+}
+
+int heightOfTree(struct node* root) {
+    if (root == NULL) {
+        return 0;
     }
-    pop(curr);
+    int leftHeight = heightOfTree(root->left);
+    int rightHeight = heightOfTree(root->right);
+    return 1 + ((leftHeight > rightHeight) ? leftHeight : rightHeight);
 }
 
-node* front(Node* head){
-    Node* curr=head;
-    if(!head){
-        return;
+void printSpace(double n, struct node* removed) {
+    for (; n > 0; n--) {
+        printf("\t");
     }
-    node* key=pop(curr);
-    return key;
+    if (removed == NULL) {
+        printf(" ");
+    } else {
+        printf("%d", removed->data);
+    }
 }
 
-void buildFromLevelOrder(node* &root){
-    Node* head=NULL;
-    printf("enter the data for root: ");
-    scanf("%d",&data);
-    root=(node*)malloc(sizeof(node));
-    push(root,data);
-    while(!empty(root)){
-        node* temp=front(root);
-        pop(root);
-        printf("enter left node fro:",temp->data);
-        int leftdata;
-        scanf("%d",&leftdata);
-        if(leftdata!=-1){
-            temp->left=
+void printTreeVisual(struct node* root) {
+    Node* treeLevel = NULL;
+    Node* temp = NULL;
+    push(&treeLevel, root);
+    int counter = 0;
+    int height = heightOfTree(root) - 1;
+    double numberOfElements = pow(2, (height + 1)) - 1;
+    while (counter <= height) {
+        struct node* removed = pop(&treeLevel);
+        if (temp == NULL) {
+            printSpace(numberOfElements / pow(2, counter + 1), removed);
+        } else {
+            printSpace(numberOfElements / pow(2, counter), removed);
+        }
+        if (removed == NULL) {
+            push(&temp, NULL);
+            push(&temp, NULL);
+        } else {
+            push(&temp, removed->left);
+            push(&temp, removed->right);
+        }
+        if (empty(treeLevel)) {
+            printf("\n\n");
+            treeLevel = temp;
+            temp = NULL;
+            counter++;
+        }
+    }
+}
+
+void buildFromLevelOrder(struct node** root) {
+    Node* head = NULL;
+    printf("Enter the data for root: ");
+    int data;
+    scanf("%d", &data);
+    *root = (struct node*)malloc(sizeof(struct node));
+    (*root)->data = data;
+    (*root)->left = NULL;
+    (*root)->right = NULL;
+    push(&head, *root);
+
+    while (!empty(head)) {
+        struct node* temp = pop(&head);
+
+        printf("Enter left node for %d (-1 for no left child): ", temp->data);
+        int leftData;
+        scanf("%d", &leftData);
+        if (leftData != -1) {
+            temp->left = (struct node*)malloc(sizeof(struct node));
+            temp->left->data = leftData;
+            temp->left->left = NULL;
+            temp->left->right = NULL;
+            push(&head, temp->left);
         }
 
-        printf("enter right node fro:",temp->data);
-        int rightdata;
-        scanf("%d",&rightdata);
-
-
+        printf("Enter right node for %d (-1 for no right child): ", temp->data);
+        int rightData;
+        scanf("%d", &rightData);
+        if (rightData != -1) {
+            temp->right = (struct node*)malloc(sizeof(struct node));
+            temp->right->data = rightData;
+            temp->right->left = NULL;
+            temp->right->right = NULL;
+            push(&head, temp->right);
+        }
     }
-
 }
 
-void printTreeInorder(struct node* root) {
+void printTreeLevelOrder(struct node* root) {
     if (root == NULL) {
         return;
     }
-    printf("%d ", root->data);
-    printTreeInorder(root->left);
-    printTreeInorder(root->right);
+
+    Node* queue = NULL;
+    push(&queue, root);
+
+    while (!empty(queue)) {
+        struct node* temp = pop(&queue);
+        printf("%d ", temp->data);
+
+        if (temp->left != NULL) {
+            push(&queue, temp->left);
+        }
+
+        if (temp->right != NULL) {
+            push(&queue, temp->right);
+        }
+    }
 }
 
 int main() {
-    struct node* root = createNode(1);
-    root->left = createNode(2);
-    root->right = createNode(3);
-    root->left->left = createNode(4);
-    printf("Nodes in the tree in inorder traversal: \n");
-    printTreeInorder(root);
+    struct node* root = NULL;
+    buildFromLevelOrder(&root);
+
+    printf("Nodes in the tree in level order traversal:\n");
+    printTreeLevelOrder(root);
+
+    printf("\n\nTree Visualization:\n");
+    printTreeVisual(root);
+
     return 0;
 }
